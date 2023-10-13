3dac12905f19381b02d1a15ccaac4a45512e2a05
Alexandre Cadima Leal <alexandre.cadima@gmail.com>
2023-10-13 19:36:28 -0300

    Add Slugify method and tests

--- tools_test.go
+++ tools_test.go
@@ -163,3 +163,31 @@ func TestTools_CreateDirIfNotExist(t *testing.T) {
 	// clean up
 	_ = os.Remove("./testdata/myDir")
 }
+
+var slugifyTests = []struct {
+	name          string
+	s             string
+	expected      string
+	errorExpected bool
+}{
+	{name: "valid string", s: "now is the time ", expected: "now-is-the-time", errorExpected: false},
+	{name: "empty string", s: "", expected: "", errorExpected: true},
+	{name: "complex string", s: "Now is the time for all GOOD men! + fish & such &ˆ123", expected: "now-is-the-time-for-all-good-men-fish-such-123", errorExpected: false},
+	{name: "japanese string", s: "こんにちは世界", expected: "", errorExpected: true},
+	{name: "japanese string and roman characters", s: "こんにちは世界 hello world", expected: "hello-world", errorExpected: false},
+}
+
+func TestTools_Slugify(t *testing.T) {
+	var testTools Tools
+
+	for _, e := range slugifyTests {
+		slug, err := testTools.Slugify(e.s)
+		if err != nil && !e.errorExpected {
+			t.Errorf("%s: error expected when none expected: %s", e.name, err.Error())
+		}
+
+		if !e.errorExpected && slug != e.expected {
+			t.Errorf("%s: wrong slug returned; expected %s but got %s", e.name, e.expected, slug)
+		}
+	}
+}
